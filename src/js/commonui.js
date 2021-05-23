f4bb958b3898cd423cfef4ff3e03d941e5e715fa
kogwing <kogwing@gmail.com>
2021-05-23 21:27:00 +0900

    param을 이용한 페이지 에니메이션 예시 작업

--- src/js/commonui.js
+++ src/js/commonui.js
@@ -139,6 +139,25 @@
             $navAnchor.removeClass('on');
             movePage(1);
         });
+
+
+        // param 추가 2021-05-23
+        function getParam(sname) {
+            var params = location.search.substr(location.search.indexOf("?") + 1);
+            var sval = "";
+            params = params.split("&");
+            for (var i = 0; i < params.length; i++) {
+                temp = params[i].split("=");
+                if ([temp[0]] == sname) { sval = temp[1]; }
+            }
+            return sval;
+        }
+
+        $(window).on('load', function(){
+            const param = Number(getParam("tab")) + 1;
+            movePage(param);
+        });
+        // param 추가 2021-05-23
     }
 
     function initVdoSlider() {
